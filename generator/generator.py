55c100146bd05c48af557feca19467e5e2f50773
GM <gao.1930@osu.edu>
2024-12-11 16:58:32 -0500

    add optimize

--- generator/generator.py
+++ generator/generator.py
@@ -14,6 +14,8 @@ class CodeGenerator:
         self.else_label_counter = 0
         self.var_usage = {}
         self.var_assignments = {}
+        self.constant_vars = {}  #  monitor constant variable --- Constant Propagation
+        self.expr_cache = {}  # common expression --- Common Elimination
 
     def new_temp(self):
         self.temp_counter += 1
@@ -76,6 +78,9 @@ class CodeGenerator:
             temp = self.generate(node.initial_value)
             self.add_instruction(f"STORE {temp}, {node.name}")
             self.var_assignments[node.name].append(len(self.instructions) - 1)
+            # check if variable is constant --- Constant Propagation
+            if isinstance(node.initial_value, Constant):
+                self.constant_vars[node.name] = node.initial_value.value
 
     def generate_assignment(self, node):
         temp = self.generate(node.value)
@@ -83,6 +88,12 @@ class CodeGenerator:
         if node.target.name not in self.var_assignments:
             self.var_assignments[node.target.name] = []
         self.var_assignments[node.target.name].append(len(self.instructions) - 1)
+        # check if value of this variable is constant --- Constant Propagation
+        if isinstance(node.value, Constant):
+            self.constant_vars[node.target.name] = node.value.value
+        else:
+            if node.target.name in self.constant_vars:
+                del self.constant_vars[node.target.name]
 
     def generate_print(self, node):
         temp = self.generate(node.expression)
@@ -151,17 +162,35 @@ class CodeGenerator:
         return temp
 
     def generate_binary_operation(self, node):
+        # check if two op is variable --- Common Elimination
         if isinstance(node.left, Constant) and isinstance(node.right, Constant):
             left_val = node.left.value
             right_val = node.right.value
             result = self.evaluate_binop(node.operator, left_val, right_val)
             return self.generate_constant(Constant(result))
         else:
-            left = self.generate(node.left)
-            right = self.generate(node.right)
-            temp = self.new_temp()
-            self.add_instruction(f"BINOP {node.operator}, {left}, {right}, {temp}")
-            return temp
+            # create a key for experssion --- Common Elimination
+            expr_key = (node.operator, self.get_operand_key(node.left), self.get_operand_key(node.right))
+            if expr_key in self.expr_cache:
+                return self.expr_cache[expr_key]
+            else:
+                left = self.generate(node.left)
+                right = self.generate(node.right)
+                temp = self.new_temp()
+                self.add_instruction(f"BINOP {node.operator}, {left}, {right}, {temp}")
+                self.expr_cache[expr_key] = temp
+                return temp
+            
+    # Common Elimination
+    def get_operand_key(self, operand):
+        if isinstance(operand, Constant):
+            return ('const', operand.value)
+        elif isinstance(operand, Identifier):
+            return ('var', operand.name)
+        elif isinstance(operand, BinaryOperation):
+            return ('expr', operand.operator, self.get_operand_key(operand.left), self.get_operand_key(operand.right))
+        else:
+            return ('unknown',)
 
     def evaluate_binop(self, operator, left, right):
         if operator == '+':
@@ -196,20 +225,23 @@ class CodeGenerator:
     def generate_constant(self, node):
         temp = self.new_temp()
         if isinstance(node.value, str):
-            value = f'"{node.value}"'  #add ""
+            value = f'"{node.value}"' 
         else:
             value = node.value
         self.add_instruction(f"LOAD_CONST {value}, {temp}")
         return temp
 
     def generate_identifier(self, node):
+        if node.name in self.constant_vars:
+            # use constant to replace variable --- Constant Propagation
+            return self.generate_constant(Constant(self.constant_vars[node.name]))
         temp = self.new_temp()
         self.add_instruction(f"LOAD {node.name}, {temp}")
         self.var_usage[node.name] = self.var_usage.get(node.name, 0) + 1
         return temp
-
-    def optimize(self):
-        print("Optimizing...")
+    
+    # optimize 2
+    def remove_dead_code(self):
         to_remove = set()
 
         for var, assignments in self.var_assignments.items():
@@ -242,6 +274,73 @@ class CodeGenerator:
             instr for idx, instr in enumerate(self.instructions)
             if idx not in to_remove
         ]
+
+    # optimize 3
+    def loop_invariant_code_motion(self):
+        optimized_instructions = []
+        i = 0
+        while i < len(self.instructions):
+            instr = self.instructions[i]
+            tokens = tokenize_instruction(instr)
+            if tokens and tokens[0] == "LABEL" and tokens[1].startswith("start_label"):
+                loop_start = i
+                # find end 
+                end_label = None
+                for j in range(i + 1, len(self.instructions)):
+                    tokens_j = tokenize_instruction(self.instructions[j])
+                    if tokens_j and tokens_j[0] == "LABEL" and tokens_j[1].startswith("end_label"):
+                        end_label = j
+                        break
+                if end_label is not None:
+                    invariants = []
+                    loop_vars = self.get_loop_variables(loop_start, end_label)
+                    for k in range(loop_start + 1, end_label):
+                        instr_k = self.instructions[k]
+                        tokens_k = tokenize_instruction(instr_k)
+                        if self.is_loop_invariant(tokens_k, loop_vars):
+                            invariants.append((k, instr_k))
+                    for (k, instr_k) in reversed(invariants):
+                        optimized_instructions.insert(loop_start, instr_k)
+                        del self.instructions[k]
+                optimized_instructions.append(instr)
+            else:
+                optimized_instructions.append(instr)
+            i += 1
+        self.instructions = optimized_instructions
+
+    def get_loop_variables(self, loop_start, loop_end):
+        loop_vars = set()
+        for k in range(loop_start, loop_end):
+            tokens_k = tokenize_instruction(self.instructions[k])
+            if tokens_k and tokens_k[0] == "STORE":
+                loop_vars.add(tokens_k[2])
+        return loop_vars
+
+    def is_loop_invariant(self, tokens, loop_vars):
+        for token in tokens[1:]:
+            if token in loop_vars:
+                return False
+        return True
+    
+    # optimize 1
+    def constant_propagation(self):
+        # aleady implement at code
+        pass
+    
+    # optimize 4
+    def common_subexpression_elimination(self):
+        # aleady implement at code
+        pass
+
+    def optimize(self):
+        print("Optimizing...")
+        self.remove_dead_code()
+        self.constant_propagation()
+        self.common_subexpression_elimination()
+        self.loop_invariant_code_motion()
+        print("Optimization complete.")
+
+       
 
     def analyze_temp_usage(self):
         temp_usage = set()
